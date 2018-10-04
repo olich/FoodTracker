c638cad255c34fe54c0c28050e080bc7482d2aaa
Olivier Champion <ochampion@mac.com>
2018-10-04 09:49:44 +0200

    Current state

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 import os.log
 
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
 class MealTableViewController: UITableViewController {
 
     //MARK: Properties
